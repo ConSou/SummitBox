2e96e53bf6d78aa014606bd6fbbd9ab50b9e95b2
Conor Souhrada <ConorSouhrada@gmail.com>
2018-08-28 20:07:31 -0600

    edit completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    if @user.image.attached?
-      image = my_url(@user)
-      @user.imgurl = "http://localhost:3000#{image}"
-    end
-
     render :show
 
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.assign_attributes(user_params)
-    #@user.image.attach(params[:image])
+    if params[:image]
+      @user = User.find(params[:id])
+      @user.assign_attributes(no_user_params)
+      @user.image.attach(params[:image])
+      image = my_url(@user)
+      @user.imgurl = "http://localhost:3000#{image}"
+    else
+      @user = User.find(params[:id])
+      @user.assign_attributes(user_params)
+    end
 
     @user.save!
-
 
     render :show
 
@@ -42,6 +43,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :image, :bio, :city, :state, :country)
     # params.fetch(:user, {}).permit(:email, :password, :password_confirmation, :first_name, :last_name, :image)
+    #.require(:user)
+  end
+
+  def no_user_params
+    params.permit(:email, :password, :password_confirmation, :first_name, :last_name, :image, :bio, :city, :state, :country)
   end
 
   def my_url(user)
